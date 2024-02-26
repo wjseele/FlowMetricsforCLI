e261009187e6961624e836cbdf1282402a23c878
Will Seele <wjseele@gmail.com>
2024-02-26 15:28:57 +0100

    Monte Carlo function seems to work.

--- main.go
+++ main.go
@@ -6,13 +6,25 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to Flow Metrics for CLI")
-	fmt.Println("Let's try a test")
+	fmt.Println("Welcome to Flow Metrics for CLI.")
+	fmt.Println("Let's try a test.")
 
 	var randomThroughputRange = fmlib.DataGenerator()
 
 	fmt.Println("Throughput for each day is:")
 	for i, n := range randomThroughputRange {
 		fmt.Println("Day", i+1, ":", n)
+	}
+
+	fmt.Println("Now let's try running a Monte Carlo simulation.")
+	fmt.Println("We'll forecast 30 days of work on our test data.")
+
+	daysToForecast := 30
+
+	var monteCarloSlice = fmlib.MonteCarlo(daysToForecast, randomThroughputRange)
+
+	fmt.Println("Our forecast is:")
+	for i, n := range monteCarloSlice {
+		fmt.Println("Amount of", i, ":", n)
 	}
 }
