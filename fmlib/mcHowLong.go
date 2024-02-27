23a17393cf1da589c3423c4e470a64ef3f33c117
Will Seele <wjseele@gmail.com>
2024-02-27 11:43:05 +0100

    Ok, How Long took some puzzling.

--- fmlib/mcHowLong.go
+++ fmlib/mcHowLong.go
@@ -1,1 +1,81 @@
 package fmlib
+
+import (
+	"math/rand"
+	"slices"
+)
+
+func HowLong(desiredAmount int, throughputRange []int) (int, int, int, int, []int) {
+	var project50, project75, project85, project95 int
+
+	var monteCarloHLSlice = howLong(desiredAmount, throughputRange)
+
+	total := 10000
+	for i := len(monteCarloHLSlice) - 1; i >= 0; i-- {
+		total -= monteCarloHLSlice[i]
+		if total >= 5000 {
+			project50 = i
+		}
+		if total >= 7500 {
+			project75 = i
+		}
+		if total >= 8500 {
+			project85 = i
+		}
+		if total >= 9500 {
+			project95 = i
+		}
+	}
+
+	return project50, project75, project85, project95, monteCarloHLSlice
+}
+
+func howLong(desiredAmount int, throughputRange []int) []int {
+	var minimumDelivery, daysToForecast int
+
+	if slices.Min(throughputRange) > 0 {
+		minimumDelivery = slices.Min(throughputRange)
+	} else {
+		minimumDelivery = 1
+	}
+
+	daysToForecast = (desiredAmount / minimumDelivery) * counts(throughputRange, 0)
+
+	var monteCarloHLSlice = monteCarloHL(desiredAmount, daysToForecast, throughputRange)
+
+	return monteCarloHLSlice
+}
+
+func counts[S []E, E comparable](s S, e E) int {
+	var n int
+	for _, v := range s {
+		if v == e {
+			n++
+		}
+	}
+	return n
+}
+
+func monteCarloHL(desiredAmount int, daysToForecast int, throughputRange []int) []int {
+	var tempTotal int
+
+	monteCarloHLSlice := make([]int, daysToForecast)
+
+	for i := 0; i < 10000; i++ {
+		tempTotal = monteCarloSingleCountdown(desiredAmount, daysToForecast, throughputRange)
+		monteCarloHLSlice[tempTotal] += 1
+	}
+
+	return monteCarloHLSlice
+}
+
+func monteCarloSingleCountdown(desiredAmount int, daysToForecast int, throughputRange []int) int {
+	days := 0
+
+	for desiredAmount > 0 {
+		desiredAmount -= throughputRange[rand.Intn(len(throughputRange))]
+		days++
+	}
+
+	return days
+}
--- main.go
+++ main.go
@@ -33,4 +33,22 @@ func main() {
 	fmt.Println("There's a 75% chance of delivering at least", project75, "items.")
 	fmt.Println("There's a 85% chance of delivering at least", project85, "items.")
 	fmt.Println("There's a 95% chance of delivering at least", project95, "items.")
+
+	fmt.Println("Now let's see how long it takes us to deliver a certain amount of items.")
+	fmt.Println("We'll aim for 60.")
+
+	desiredAmount := 60
+
+	var howlong50, howlong75, howlong85, howlong95, monteCarloHLSlice = fmlib.HowLong(desiredAmount, randomThroughputRange)
+
+	fmt.Println("Our forecast is:")
+	for i, n := range monteCarloHLSlice {
+		fmt.Println("It took", i, "days in", n, "cases.")
+	}
+
+	fmt.Println("Now let's see how many days we should expect.")
+	fmt.Println("There's a 50% chance to deliver", desiredAmount, "in", howlong50, "days.")
+	fmt.Println("There's a 75% chance to deliver", desiredAmount, "in", howlong75, "days.")
+	fmt.Println("There's a 85% chance to deliver", desiredAmount, "in", howlong85, "days.")
+	fmt.Println("There's a 95% chance to deliver", desiredAmount, "in", howlong95, "days.")
 }
