30e0726aeecaf6e35890c40387b66a46d142c41e
Will Seele <wjseele@gmail.com>
2024-02-27 11:52:14 +0100

    Cleaned up the structure, removed separate Monte Carlo function.

--- fmlib/mcHowMany.go
+++ fmlib/mcHowMany.go
@@ -1,12 +1,17 @@
 package fmlib
 
+import (
+	"math/rand"
+	"slices"
+)
+
 func HowMany(daysToForecast int, throughputRange []int) (int, int, int, int, []int) {
 	var project50, project75, project85, project95 int
-	var monteCarloSlice = MonteCarlo(daysToForecast, throughputRange)
+	var monteCarloHMSlice = monteCarloHM(daysToForecast, throughputRange)
 
 	total := 10000
-	for i := 0; i < len(monteCarloSlice); i++ {
-		total -= monteCarloSlice[i]
+	for i := 0; i < len(monteCarloHMSlice); i++ {
+		total -= monteCarloHMSlice[i]
 		if total >= 5000 {
 			project50 = i
 		}
@@ -21,5 +26,28 @@ func HowMany(daysToForecast int, throughputRange []int) (int, int, int, int, []i
 		}
 	}
 
-	return project50, project75, project85, project95, monteCarloSlice
+	return project50, project75, project85, project95, monteCarloHMSlice
+}
+
+func monteCarloHM(daysToForecast int, throughputRange []int) []int {
+	var tempTotal int
+
+	monteCarloHMSlice := make([]int, (slices.Max(throughputRange)*daysToForecast)+1)
+
+	for i := 0; i < 10000; i++ {
+		tempTotal = monteCarloSingleForecast(daysToForecast, throughputRange)
+		monteCarloHMSlice[tempTotal] += 1
+	}
+
+	return monteCarloHMSlice
+}
+
+func monteCarloSingleForecast(daysToForecast int, throughputRange []int) int {
+	total := 0
+
+	for i := 0; i < daysToForecast; i++ {
+		total += throughputRange[rand.Intn(len(throughputRange))]
+	}
+
+	return total
 }
