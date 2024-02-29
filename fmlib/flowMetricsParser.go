c5c8f8efd34018b4709664f4cd3042a78af513f3
Will Seele <wjseele@gmail.com>
2024-02-29 13:34:08 +0100

    Starting on the parser.

--- fmlib/flowMetricsParser.go
+++ fmlib/flowMetricsParser.go
@@ -1,1 +1,18 @@
 package fmlib
+
+import ()
+
+type item struct {
+	ID       int64
+	name     string
+	created  int
+	state1   int
+	state2   int
+	state3   int
+	finished int
+	blocked  int
+}
+
+func Parser() {
+
+}
