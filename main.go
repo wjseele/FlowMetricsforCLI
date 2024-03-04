bb0bd723cafd82f0c92eed5e1c11e54b4ce43262
Will Seele <wjseele@gmail.com>
2024-03-04 13:30:01 +0100

    Added a selection mode in the main menu.

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/wjseele/FlowMetricsforCLI/fmlib"
 )
 
@@ -9,6 +11,19 @@ func main() {
 	fmt.Println("Welcome to Flow Metrics for CLI.")
 	fmt.Println("There's currently only a demo.")
 
-	fmlib.Demo()
-	fmlib.Parser()
+	selection := 0
+
+	fmt.Println("Select what you'd like to test.")
+	fmt.Println("1: Demo, 2: Parser, 0: Exit")
+	fmt.Scanln(&selection)
+
+	if selection == 1 {
+		fmlib.Demo()
+	}
+	if selection == 2 {
+		fmlib.Parser()
+	}
+	if selection == 0 {
+		os.Exit(0)
+	}
 }
