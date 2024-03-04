c220233ecc91940e65a6b6ccf4288af1455c9418
Will Seele <wjseele@gmail.com>
2024-03-04 13:56:04 +0100

    Updated the selection mode.

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/wjseele/FlowMetricsforCLI/fmlib"
 )
 
@@ -11,19 +9,22 @@ func main() {
 	fmt.Println("Welcome to Flow Metrics for CLI.")
 	fmt.Println("There's currently only a demo.")
 
-	selection := 0
+	selection := 10
 
-	fmt.Println("Select what you'd like to test.")
-	fmt.Println("1: Demo, 2: Parser, 0: Exit")
-	fmt.Scanln(&selection)
+	for selection != 0 {
+		fmt.Println("Select what you'd like to test.")
+		fmt.Println("1: Demo, 2: Parser, 0: Exit")
+		_, err := fmt.Scanln(&selection)
 
-	if selection == 1 {
-		fmlib.Demo()
-	}
-	if selection == 2 {
-		fmlib.Parser()
-	}
-	if selection == 0 {
-		os.Exit(0)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if selection == 1 {
+			fmlib.Demo()
+		}
+		if selection == 2 {
+			fmlib.Parser()
+		}
 	}
 }
