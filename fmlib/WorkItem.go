bfd4d73f7403d85abfe7095845462d8544b7b44f
Rich Visotcky <visotcky@gmail.com>
2024-03-17 15:41:58 -0500

    Fix cycle time by day only, add method for cycle time between two states

--- fmlib/WorkItem.go
+++ fmlib/WorkItem.go
@@ -28,11 +28,50 @@ func (wi WorkItem) States() []WorkState {
 	return wi.states
 }
 
-func (wi WorkItem) CycleTime() int {
+func (wi WorkItem) retrieveWorkState(stateName string) WorkState {
+	index := wi.findWorkState(stateName)
+
+	return wi.states[index]
+}
+
+func (wi WorkItem) findWorkState(stateName string) int {
+	for i, item := range wi.states {
+		if item.stateName == stateName {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func (wi WorkItem) CycleTimeMax() int {
 	first := wi.states[0].date
 	last := wi.states[len(wi.states)-1].date
 
-	diff := last.Sub(first).Abs()
+	return cycleTime(first, last)
+}
 
-	return int(math.Ceil(diff.Hours() / 24))
+func truncateToDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
+
+func cycleTime(first time.Time, last time.Time) int {
+	timestampOne := truncateToDay(first)
+	timestampTwo := truncateToDay(last)
+
+	diff := timestampTwo.Sub(timestampOne).Abs()
+	ct := int(math.Ceil(diff.Hours() / 24))
+
+	if ct == 0 && !first.Equal(last) {
+		ct = 1
+	}
+
+	return ct
+}
+
+func (wi WorkItem) CycleTime(stateOne string, stateTwo string) int {
+	first := wi.retrieveWorkState(stateOne)
+	second := wi.retrieveWorkState(stateTwo)
+
+	return cycleTime(first.date, second.date)
 }
--- fmlib/WorkItem_test.go
+++ fmlib/WorkItem_test.go
@@ -5,43 +5,43 @@ import (
 	"time"
 )
 
-func TestCycleTime_TwoValuesAreSame_ReturnsZero(t *testing.T) {
+func TestCycleTimeMax_TwoValuesAreSame_ReturnsZero(t *testing.T) {
 	day := time.Date(2030, time.May, 10, 0, 0, 0, 0, time.UTC)
 	item := WorkItem{1, "item1", []WorkState{{"start", day}, {"stop", day}}}
 
-	cycleTime := item.CycleTime()
+	cycleTime := item.CycleTimeMax()
 
 	if cycleTime != 0 {
 		t.Errorf("Cycle time was not equal to zero for same time completion. Expected: 0; Actual %d", cycleTime)
 	}
 }
 
-func TestCycleTime_TwoTimesAreHoursApart_ReturnsOne(t *testing.T) {
+func TestCycleTimeMax_TwoTimesAreHoursApart_ReturnsOne(t *testing.T) {
 	day1 := time.Date(2030, time.May, 10, 0, 0, 0, 0, time.UTC)
 	day2 := time.Date(2030, time.May, 10, 5, 0, 0, 0, time.UTC)
 	item := WorkItem{1, "item1", []WorkState{{"start", day1}, {"stop", day2}}}
 
-	cycleTime := item.CycleTime()
+	cycleTime := item.CycleTimeMax()
 
 	if cycleTime != 1 {
 		t.Errorf("Cycle time was not equal to one for same day completion. Expected: 1; Actual %d", cycleTime)
 	}
 }
 
-func TestCycleTime_TwoDifferentDates_ReturnsDaysDifference(t *testing.T) {
+func TestCycleTimeMax_TwoDifferentDates_ReturnsDaysDifference(t *testing.T) {
 	day1 := time.Date(2030, time.August, 5, 0, 0, 0, 0, time.UTC)
 	day2 := time.Date(2030, time.August, 8, 0, 0, 0, 0, time.UTC)
 	item := WorkItem{1, "item1", []WorkState{{"start", day1}, {"stop", day2}}}
 	expected := 3
 
-	cycleTime := item.CycleTime()
+	cycleTime := item.CycleTimeMax()
 
 	if cycleTime != expected {
 		t.Errorf("Cycle time mismatch. Expected: %d; Actual: %d", expected, cycleTime)
 	}
 }
 
-func TestCycleTime_ManyStates_ReturnsDifferenceBetweenFirstAndLast(t *testing.T) {
+func TestCycleTimeMax_ManyStates_ReturnsDifferenceBetweenFirstAndLast(t *testing.T) {
 	day1 := time.Date(2030, time.March, 29, 10, 0, 0, 0, time.UTC)
 	day2 := time.Date(2030, time.April, 1, 5, 0, 0, 0, time.UTC)
 	day3 := time.Date(2030, time.April, 2, 14, 0, 0, 0, time.UTC)
@@ -50,50 +50,66 @@ func TestCycleTime_ManyStates_ReturnsDifferenceBetweenFirstAndLast(t *testing.T)
 	item := WorkItem{1, "item1", []WorkState{{"state1", day1}, {"state2", day2}, {"state3", day3}, {"state4", day4}}}
 	expected := 6
 
-	cycleTime := item.CycleTime()
+	cycleTime := item.CycleTimeMax()
 
 	if cycleTime != expected {
 		t.Errorf("Cycle time mismatch. Expected: %d; Actual: %d", expected, cycleTime)
 	}
 }
 
-func TestCycleTime_DatesAreDifferentTimezones_CycleTimeAccountsForTimezones(t *testing.T) {
+func TestCycleTimeMax_DatesAreDifferentTimezones_CycleTimeAccountsForTimezones(t *testing.T) {
 	day1 := time.Date(2030, time.August, 5, 11, 0, 0, 0, time.UTC)
 	day2 := time.Date(2030, time.August, 11, 11, 0, 0, 0, time.FixedZone("EastlyTime", -7))
 
 	item := WorkItem{1, "item1", []WorkState{{"start", day1}, {"stop", day2}}}
 	expected := 7 // Expected because of the timezone offset
 
-	cycleTime := item.CycleTime()
+	cycleTime := item.CycleTimeMax()
 
 	if cycleTime != expected {
 		t.Errorf("Cycle time mismatch. Expected: %d; Actual: %d", expected, cycleTime)
 	}
 }
 
-func TestCycleTime_DatesAreReversed_GivesAbsoluteValue(t *testing.T) {
+func TestCycleTimeMax_DatesAreReversed_GivesAbsoluteValue(t *testing.T) {
 	day1 := time.Date(2030, time.August, 3, 11, 0, 0, 0, time.UTC)
 	day2 := time.Date(2030, time.August, 14, 0, 0, 0, 0, time.UTC)
 
 	item := WorkItem{1, "item1", []WorkState{{"start", day2}, {"stop", day1}}} // Note that day2 comes first
 	expected := 11
 
-	cycleTime := item.CycleTime()
+	cycleTime := item.CycleTimeMax()
 
 	if cycleTime != expected {
 		t.Errorf("Cycle time mismatch. Expected: %d; Actual: %d", expected, cycleTime)
 	}
 }
 
-func TestCycleTime_LeapYearDates_IncludesFeb29(t *testing.T) {
+func TestCycleTimeMax_LeapYearDates_IncludesFeb29(t *testing.T) {
 	day1 := time.Date(2024, time.February, 27, 8, 0, 0, 0, time.UTC)
 	day2 := time.Date(2024, time.March, 2, 7, 0, 0, 0, time.UTC)
 	item := WorkItem{1, "item1", []WorkState{{"start", day1}, {"stop", day2}}}
 	expected := 4
 
-	cycleTime := item.CycleTime()
+	cycleTime := item.CycleTimeMax()
 
 	if cycleTime != expected {
 		t.Errorf("Cycle time mismatch. Expected: %d; Actual: %d", expected, cycleTime)
+	}
+}
+
+func TestCycleTime_BetweenTwoStates_ReturnsDaysDifference(t *testing.T) {
+	day1 := time.Date(2030, time.March, 29, 10, 0, 0, 0, time.UTC)
+	day2 := time.Date(2030, time.April, 1, 5, 0, 0, 0, time.UTC)
+	day3 := time.Date(2030, time.April, 2, 14, 0, 0, 0, time.UTC)
+	day4 := time.Date(2030, time.April, 4, 8, 0, 0, 0, time.UTC)
+
+	item := WorkItem{1, "item1", []WorkState{{"state1", day1}, {"state2", day2}, {"state3", day3}, {"state4", day4}}}
+	expectedCycleTime := 4
+
+	cycleTime := item.CycleTime("state1", "state3")
+
+	if cycleTime != expectedCycleTime {
+		t.Errorf("Cycle time mismatch. Expected: %d; Actual: %d", expectedCycleTime, cycleTime)
 	}
 }
